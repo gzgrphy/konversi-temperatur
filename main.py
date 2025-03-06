ab917d1110f80f299041f6ed30a2852e3477605a
geza nurhalim <gezanurhalimm@gmail.com>
2025-03-06 07:52:58 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+celsius = float(input("Masukkan suhu dalam derajat Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"{celsius} derajat Celsius sama dengan {fahrenheit:.2f} derajat Fahrenheit")
